c46ab051871c54fc10c4fbef6bead503d263c872
Claire Yang <39376681+yangjy0826@users.noreply.github.com>
2018-06-18 23:20:42 +0800

    Add files via upload

---
+++ array.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+# import numpy as np
+
+
+class Solution:
+    # array 二维列表
+    # def Find(self, target, array):
+    def __init__(self, target, array):
+        # write code here
+        self.target = target
+        self.array = array
+
+    def Find(self):
+        c = 0
+        for i in range(len(self.array)):
+            for j in range(len(self.array[0])):
+                if self.array[i][j] == self.target:
+                    c += 1
+        if c == 0:
+            return False
+        else:
+            return True
+
+
+tar = 7
+# arr = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6]])
+arr = [[1, 2, 8, 9], [4, 7, 10, 13]]
+obj = Solution(tar, arr)
+Result = obj.Find()
+print(Result)
+'''
+c = 0
+# for i in range(obj.array.shape[0]):
+# for j in range(obj.array.shape[1]):
+for i in range(len(obj.array)):
+    for j in range(len(obj.array[0])):
+        if obj.array[i][j] == obj.target:
+            c += 1
+if c == 0:
+    print("false")
+else:
+    print("true")
+'''
