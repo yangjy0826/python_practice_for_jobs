29e8cd3916c2efd2372052cd41a28c4d03c0a2a2
Claire Yang <39376681+yangjy0826@users.noreply.github.com>
2018-08-07 08:40:36 +0800

    Add files via upload

---
+++ 剑指offer/Fibonacci.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def Fibonacci(self, n):
+        # write code here
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            p1 = 0
+            p2 = 1
+            for i in range(2, n+1):  # 依次取2,3,4,5,...,n
+                out = p1+p2
+                p1 = p2
+                p2 = out
+            return out
