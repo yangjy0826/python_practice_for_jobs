e91f1e8a1354b895ddcedb3af1aa8df0f9fe5018
Claire Yang <39376681+yangjy0826@users.noreply.github.com>
2018-09-06 21:21:01 +0200

    Add files via upload

---
+++ Leetcode/617. Merge Two Binary Trees.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mergeTrees(self, t1, t2): ##recursion
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if t1 == None:
+            return t2
+        if t2 == None:
+            return t1
+        if t1 == None and t2 == None:
+            return None
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left,t2.left)
+        t1.right = self.mergeTrees(t1.right,t2.right)
+        return t1
