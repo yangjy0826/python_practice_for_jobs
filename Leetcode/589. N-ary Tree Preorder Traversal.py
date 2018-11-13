d22773a48a0d070b2854cabba2650dcfc3f0c482
Claire Yang <39376681+yangjy0826@users.noreply.github.com>
2018-11-13 15:29:45 +0100

    Update Leetcode/589. N-ary Tree Preorder Traversal.py

--- Leetcode/589. N-ary Tree Preorder Traversal.py
+++ Leetcode/589. N-ary Tree Preorder Traversal.py
@@ -1,1 +1,31 @@
 # Given an n-ary tree, return the preorder traversal of its nodes' values.
+# The 1st solution:
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+
+        stack, output = [root, ], []
+        while stack:
+            root = stack.pop()
+            output.append(root.val)
+            print(root.children[::-1])
+
+            stack.extend(root.children[::-1])
+        return output
+root={"$id":"1","children":[{"$id":"2","children":[{"$id":"5","children":[],"val":5},{"$id":"6","children":[],"val":6}],"val":3},{"$id":"3","children":[],"val":2},{"$id":"4","children":[],"val":4}],"val":1}
+Solution.preorder(root)
+# The 2nd solution:
